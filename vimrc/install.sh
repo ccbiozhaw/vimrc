efe9cda3873425fa10901e46952f624b3405af89
ccbiozhaw <patc@zhaw.ch>
2021-12-31 21:52:09 +0000

    fixed setup

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-
 mkdir -p ~/.vim/plugin
+
+
+rm -rf ~/.vimrc
+ln -s ~/dotfiles/vimrc/vimrc/.vimrc ~/.vimrc
 
 # vim sources in order of files, so 1,2,3 at beginning defines order
 # check order: :scriptnames
