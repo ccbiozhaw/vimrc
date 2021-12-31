5a135b1193083ea35fdc3070f0192bf4ae96682a
ccbiozhaw <patc@zhaw.ch>
2021-12-31 16:33:25 +0000

    cleanup

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+
 mkdir -p ~/.vim/plugin
 
+# vim sources in order of files, so 1,2,3 at beginning defines order
+# check order: :scriptnames
 for f in find *.vim
 do
     rm -rf ~/.vim/plugin/$f
